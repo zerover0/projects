84a84466d7c18f0c0d04baddf13852bb29c354ae
Hans Kim <sanghankim@gmail.com>
2016-06-27 14:11:47 +0900

    Add files via upload

---
+++ opentsdb/start-servers.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# JAVA
+export JAVA_HOME=/usr/lib/jvm/default-java
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Hadoop
+export HADOOP_HOME=/home/hadoop/app/hadoop
+export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH
+
+echo "Starting Hadoop servers..."
+/home/hadoop/app/hadoop/bin/start-dfs.sh
+/home/hadoop/app/hadoop/bin/start-mapred.sh
+echo "Waiting for Hadoop ready..."
+sleep 30
+
+# HBase
+export HBASE_HOME=/home/hadoop/app/hbase
+export PATH=$HBASE_HOME/bin:$PATH
+
+echo "Starting HBase servers..."
+/home/hadoop/app/hbase/bin/start-hbase.sh
+echo "Waiting for HBase ready..."
+sleep 60
+
+# OpenTSDB
+echo "Starting HBase servers..."
+/home/hadoop/app/opentsdb/build/tsdb tsd --config=/home/hadoop/app/opentsdb/src/opentsdb.conf&
+echo $! > /home/hadoop/data/opentsdb/opentsdb.pid
+
---
+++ opentsdb/stop-servers.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# JAVA
+export JAVA_HOME=/usr/lib/jvm/default-java
+export PATH=$JAVA_HOME/bin:$PATH
+
+# OpenTSDB
+kill -HUP `cat /home/hadoop/data/opentsdb/opentsdb.pid`
+
+# HBase
+export HBASE_HOME=/home/hadoop/app/hbase
+export PATH=$HBASE_HOME/bin:$PATH
+
+/home/hadoop/app/hbase/bin/stop-hbase.sh
+
+# Hadoop
+export HADOOP_HOME=/home/hadoop/app/hadoop
+export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH
+
+/home/hadoop/app/hadoop/bin/stop-mapred.sh
+/home/hadoop/app/hadoop/bin/stop-dfs.sh
+
